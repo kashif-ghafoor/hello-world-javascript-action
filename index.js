82e687c8fc90ba6e17886c27c85022f09136a823
kashif-ghafoor <kashifghafoor140@gmail.com>
2023-12-25 22:55:59 +0500

    testing secrets

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
+  const notionToken = core.getInput("NOTION_TOKEN");
+  console.log(`NOTION_TOKEN ${notionToken}`);
+  const githubToken = process.env.GIT_TOKEN;
+  console.log(`GIT_TOKEN ${githubToken}`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
